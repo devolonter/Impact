458dc6cc815eb9de8f4e93514b650b81301be9f8
devolonter <devolonter@gmail.com>
2011-05-03 23:54:23 +0400

    Add .gitignore, add NavigateToUrl function in util module for android (i'm not sure that it's working), add android template

---
+++ modules/horizon/native/util.android.java
@@ -0,0 +1,9 @@
+import android.net.Uri;
+
+class util {
+	static void NavigateToUrl(String url) {		
+		Intent i = new Intent(Intent.ACTION_VIEW);
+		i.setData(Uri.parse(url));
+		MonkeyGame.activity.startActivity(i);
+	}
+}
